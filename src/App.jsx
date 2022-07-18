8215e1c08fcb868834b93ca48b7148f8bb785343
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-07-18 23:44:37 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
       </Routes> */}
       
-      <Footer />
+      {/* <Footer /> */}
      
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./Navbar.module.css";
 
 
 function Navbar() {
 
   return (
     <div className="mainNavbar">
+      <div>
+        Free shipping over $89, or shop online and pick up select orders at a Nordstrom Rack or Nordstrom store. Learn More Search
+      </div>
       <div className="navTop">
           <span>    
-            <Link className="auth" to="/login">Login | </Link> 
-            <Link className="auth" to="/register">Register</Link>
+            <Link className="auth" to="/login">Sign In</Link> 
+            {/* <Link className="auth" to="/register">Register</Link> */}
           </span>
       </div>
 
       <div className="navMid">
           <div className="midLeft">
-              <Link className="homeicon" to="/"> <img src="https://www.naturesbasket.co.in/Images/logosnew.png?v=2" alt="natureBasket" /></Link>
+              <Link className="homeicon" to="/"> <img src="https://n.nordstrommedia.com/alias/nordstrom-rack-logo.svg" alt="nordstromrack" /></Link>
           </div>
 
           <div className="midRight">
               <div className="midSearch">
-                  <input className="Nav-searchbar" type="text" name="search" placeholder="Start shopping" />
+                  <input className="Nav-searchbar" type="text" name="search" placeholder="Search for product and brand" />
               </div> 
           </div>
       </div>
@@ -37,13 +40,16 @@ function Navbar() {
               fontSize: "20px",
               border: "none"
             }}>SHOP BY CATEGORY</button>
-          <Link className="nav-link" to="/"><button>MY ORDERS</button></Link>
-          <Link className="nav-link" to="/gifting"><button>GIFTING</button></Link>
-          <Link className="nav-link" to="/rewards"><button>REWARDS</button></Link>
-          <Link className="nav-link" to="/blog"><button>BLOG</button></Link>
-          <Link className="nav-link" to="/product"><button>OFFERS</button></Link>
-          <Link className="nav-link" to="/cselection"><button>CONNOISSEUR'S SELECTION</button></Link>
-          <Link className="nav-link" to="/bsvisit"><button>BOOK STORE VISIT</button></Link>
+          <Link className="nav-link" to="/"><button>Clearence</button></Link>
+          <Link className="nav-link" to="/women"><button>Women</button></Link>
+          <Link className="nav-link" to="/men"><button>Men</button></Link>
+          <Link className="nav-link" to="/kids"><button>Kids</button></Link>
+          <Link className="nav-link" to="/shoes"><button>Shoes</button></Link>
+          <Link className="nav-link" to="/activewear"><button>Activewear</button></Link>
+          <Link className="nav-link" to="/bag"><button>Bag & Acessres</button></Link>
+          <Link className="nav-link" to="/home"><button>Home</button></Link>
+          <Link className="nav-link" to="/beauty"><button>Beauty</button></Link>
+          <Link className="nav-link" to="/gift"><button>Gift</button></Link>
       </div>
     </div>
   )
